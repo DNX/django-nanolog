7d8d6306d6ddb0efd57aa945e2865c4cbc280116
DNX <denis.darii@gmail.com>
2014-07-19 20:05:53 +0200

    added new fields and the initian migrations

--- nanolog/models.py
+++ nanolog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
 
 
 class Nanolog(models.Model):
